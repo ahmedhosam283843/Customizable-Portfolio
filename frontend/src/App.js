98b987f3690cf0176afdb51b7aa6983834fd6754
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-04-10 22:11:34 +0200

    intialize react frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import Projects from './sections/Projects/Projects';
+import Experience from './sections/Experience/Experience';
+import Home from './sections/Home/Home';
+import NavBar from './components/NavBar/NavBar';
+import './App.scss';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      
+      <NavBar/>
+      <Home/>
+      <Projects/>
+      <Experience/>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,14 @@
+import './NavBar.scss'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+      <div>
+        
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
