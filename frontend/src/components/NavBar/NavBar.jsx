09f11236b91b0ce880e08668bc977271fb793501
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-04-10 23:18:59 +0200

    feat: create navbar links

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,26 @@
 import './NavBar.scss'
-import React from 'react'
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+import { HiX, HiMenuAlt4 } from 'react-icons/hi';
 
 const NavBar = () => {
+
   return (
-    <nav>
-      <div>
-        
+    <nav className='app_navbar'>
+      <div className='app_navbar_name'>
+        <p>Ah <span>med</span></p>
       </div>
+
+      <ul className="app_navbar_links">
+        {['home',  'projects', 'experience'].map((item) => (
+          <li className="p-text app_flex" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+
     </nav>
   )
 }
