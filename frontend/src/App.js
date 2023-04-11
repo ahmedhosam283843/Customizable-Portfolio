5a2419726c0274ea4d7cec20dfb8c76f2c960326
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-04-11 02:49:40 +0200

    feat: create home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import Projects from './sections/Projects/Projects';
 import Experience from './sections/Experience/Experience';
-import Home from './sections/Home/Home';
 import NavBar from './components/NavBar/NavBar';
+import Home from './sections/Home/Home';
 import './App.scss';
 
 const App = () => {
---
+++ frontend/src/constants/images.js
@@ -0,0 +1,13 @@
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+import flutter from '../assets/flutter.png';
+import python from '../assets/python.png';
+import javascript from '../assets/javascript.png';
+
+export default {
+  profile,
+  circle,
+  flutter,
+  python,
+  javascript
+};
