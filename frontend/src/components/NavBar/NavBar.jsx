d2906fd9868a65785ac201d510a60f4572146e93
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-04-12 15:09:17 +0200

    fix: name logo size

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -2,31 +2,34 @@ import './NavBar.scss'
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { HiX, HiMenuAlt4 } from 'react-icons/hi';
+import { images } from '../../constants';
+
 
 const NavBar = () => {
   const [toggle, setToggle] = useState(false);
   return (
-    <nav className='app_navbar'>
+    <nav className="app__navbar">
+
       <div className='app_navbar_name'>
         <p>Ah <span>med</span></p>
       </div>
 
-      <ul className="app_navbar_links">
+      <ul className="app__navbar-links">
         {['home', 'projects', 'experience'].map((item) => (
-          <li className="p-text app_flex" key={`link-${item}`}>
+          <li className="app__flex p-text" key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
       </ul>
 
-      <div className="app_navbar_menu">
+      <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
 
         {toggle && (
           <motion.div
-            whileInView={{ x: [280, 0] }}
-            transition={{ ease: 'easeOut', duration: 0.9 }}
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
@@ -42,7 +45,8 @@ const NavBar = () => {
         )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+
+export default NavBar;
