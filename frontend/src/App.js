faf229be2cc8e50aa2c5c0d7ae21a45669f9aa61
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-04-12 21:26:21 +0200

    feat: add navigation router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import Projects from './sections/Projects/Projects';
-import Experience from './sections/Experience/Experience';
-import NavBar from './components/NavBar/NavBar';
-import Home from './sections/Home/Home';
-import './App.scss';
-
+import Projects from "./pages/Projects/Projects.jsx";
+import Experience from "./pages/Experience/Experience.jsx";
+import NavBar from "./components/NavBar/NavBar";
+import Home from "./pages/Home/Home";
+import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/login.jsx";
+import Register from "./pages/signup/register.jsx";
+import CustomizePortofolio from "./pages/customizePortofolio/customizePortofolio.jsx";
+import NavbarSwithcer from "./components/navbarSwitcher/navbarSwitcher.jsx";
 const App = () => {
   return (
-    <div className='app'>
-      
-      <NavBar/>
-      <Home/>
-      <Projects/>
-      <Experience/>
-      
-    </div>
-  )
-}
+    <Router>
+      <NavbarSwithcer>
+        <NavBar />
+      </NavbarSwithcer>
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route
+          path="/customize-profile"
+          element={<CustomizePortofolio />}
+        ></Route>
+        <Route path="/projects" element={<Projects />}></Route>
+        <Route path="/experience" element={<Experience />}></Route>
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
