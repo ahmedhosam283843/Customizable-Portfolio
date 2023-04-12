4a0d26d1aa95b4e8eaf4a1f3d43234a89525d987
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-04-12 20:48:16 +0200

    refactor: navbar

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,43 +1,42 @@
-import './NavBar.scss'
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
-import { HiX, HiMenuAlt4 } from 'react-icons/hi';
-import { images } from '../../constants';
-
+import "./NavBar.scss";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { HiX, HiMenuAlt4 } from "react-icons/hi";
 
 const NavBar = () => {
   const [toggle, setToggle] = useState(false);
   return (
-    <nav className="app__navbar">
-
-      <div className='app_navbar_name'>
-        <p>Ah <span>med</span></p>
+    <nav className="app-navbar">
+      <div className="app-navbar-name">
+        <p>
+          Ah <span>med</span>
+        </p>
       </div>
 
-      <ul className="app__navbar-links">
-        {['home', 'projects', 'experience'].map((item) => (
-          <li className="app__flex p-text" key={`link-${item}`}>
+      <ul className="app-navbar-links">
+        {["home", "projects", "experience", "login"].map((item) => (
+          <li className="app_flex p-text" key={`link-${item}`}>
             <div />
-            <a href={`#${item}`}>{item}</a>
+            <Link to={"/" + item}>{item}</Link>
           </li>
         ))}
       </ul>
 
-      <div className="app__navbar-menu">
+      <div className="app-navbar-menubar">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
-
         {toggle && (
           <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
+            whileInView={{ x: [290, 0] }}
+            transition={{ duration: 0.95, ease: "easeInOut" }}
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {['home', 'projects', 'experience'].map((item) => (
+              {["home", "projects", "experience", "login"].map((item) => (
                 <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                  <Link to={"/" + item} onClick={() => setToggle(false)}>
                     {item}
-                  </a>
+                  </Link>
                 </li>
               ))}
             </ul>
@@ -47,6 +46,5 @@ const NavBar = () => {
     </nav>
   );
 };
-
 
 export default NavBar;
