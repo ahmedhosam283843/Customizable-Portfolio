1ddbdaa43837f2b83640dfb7f62aef8329e015d0
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-04-12 21:25:34 +0200

    feat: add skills assets

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -3,11 +3,41 @@ import circle from '../assets/circle.svg';
 import flutter from '../assets/flutter.png';
 import python from '../assets/python.png';
 import javascript from '../assets/javascript.png';
+import react from '../assets/react.png';
+import cpp from '../assets/cpp.png';
+import git from '../assets/git.png';
+
+function getImage(name) {
+  switch (name) {
+    case 'git':
+      return git;
+    case 'profile':
+      return profile;
+    case 'circle':
+      return circle;
+    case 'flutter':
+      return flutter;
+    case 'python':
+      return python;
+    case 'javascript':
+      return javascript;
+    case 'react':
+      return react;
+    case 'cpp':
+      return cpp;
+    default:
+      return null;
+  }
+}
 
 export default {
   profile,
   circle,
   flutter,
   python,
-  javascript
+  react,
+  cpp,
+  git,
+  javascript,
+  getImage,
 };
