4524935e7229a0ffe6c2fca4a9631638db44cb92
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-04-18 13:46:33 +0200

    feat: handle popup close

--- frontend/src/components/Popup/Popup.jsx
+++ frontend/src/components/Popup/Popup.jsx
@@ -1,16 +1,31 @@
 import React from "react";
-import { Dialog, DialogTitle, DialogContent } from "@material-ui/core";
+import { Dialog, DialogTitle, DialogContent} from "@material-ui/core";
+import Slide from '@material-ui/core/Slide';
+
+
+
 
 export default function Popup(props) {
   const { title, children, openPopup, setOpenPopup } = props;
+  const handleClose = () => {
+    setOpenPopup(false);
+  };
   return (
-    <Dialog open={openPopup}>
+    <Dialog
+      open={openPopup}
+      onClose={handleClose}
+      maxWidth="sm"
+      fullWidth="true"
+      TransitionComponent={Slide}
+      TransitionProps={{
+        direction: "up",
+        timeout: 500,
+      }}
+    >
       <DialogTitle>
         <div>Add New Project</div>
       </DialogTitle>
-      <DialogContent dividers>
-        {children}
-      </DialogContent>
+      <DialogContent dividers>{children}</DialogContent>
     </Dialog>
   );
 }
