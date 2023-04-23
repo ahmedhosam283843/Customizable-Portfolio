7aceae55d054dd8ec582236e65bf916b7b8aa81e
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-04-23 23:49:28 +0200

    fix: enable cors on server side

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
-import {skillRouter, loginRouter, projectRouter, portfolioRouter, userRouter, experienceRouter} from "./routers/index.js";
+import {
+  skillRouter,
+  loginRouter,
+  projectRouter,
+  portfolioRouter,
+  userRouter,
+  experienceRouter,
+} from "./routers/index.js";
 const app = express();
 const PORT = 5000;
+
+// enable CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // allow requests from any origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization" // add Authorization header here
+  ); // allow the headers that the client sends
+  next();
+});
 
 app.use(bodyParser.json());
 
