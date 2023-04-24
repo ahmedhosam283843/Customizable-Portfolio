b4bfec2ccc275b05caa713e7eac6cf1a45af78d7
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-04-24 16:16:08 +0200

    fix: add missing attribute in portfolio query

--- backend/db/db_queries/portfolio_queries.js
+++ backend/db/db_queries/portfolio_queries.js
@@ -1,39 +1,39 @@
 import pool from "../db_pool.cjs";
 
-async function getPortfolioById(request, response){
-    const id = parseInt(request.user);
+async function getPortfolioById(request, response) {
+  const id = parseInt(request.user);
 
-    pool.query(
-      "SELECT * FROM customize_portfolio WHERE user_id = $1",
-      [id],
-      (error, results) => {
-        if (error) {
-          throw error;
-        }
-        response.status(200).json(results.rows);
+  pool.query(
+    "SELECT * FROM customize_portfolio WHERE user_id = $1",
+    [id],
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-    );
-  }
+      response.status(200).json(results.rows);
+    }
+  );
+}
 
   
 
   async function createPortfolio (request, response) {
-    const user_id = parseInt(request.user);
+  const user_id = parseInt(request.user);
     const {job_title, image_url, main_skill_1, main_skill_2, main_skill_3 } =
-      request.body;
+    request.body;
 
-    pool.query(
-      "INSERT INTO customize_portfolio (job_title, user_id, image_url, main_skill_1, main_skill_2, main_skill_3) VALUES ($1, $2, $3, $4, $5)",
-      [job_title, user_id, image_url, main_skill_1, main_skill_2, main_skill_3],
+  pool.query(
+    "INSERT INTO customize_portfolio (job_title, user_id, image_url, main_skill_1, main_skill_2, main_skill_3) VALUES ($1, $2, $3, $4, $5, $6)",
+    [job_title, user_id, image_url, main_skill_1, main_skill_2, main_skill_3],
 
-      (error, results) => {
-        if (error) {
-          throw error;
-        }
-        response.status(201).send(`Portfolio added successfully`);
+    (error, results) => {
+      if (error) {
+        throw error;
       }
-    );
-  }
+      response.status(201).send(`Portfolio added successfully`);
+    }
+  );
+}
 
   export{
     getPortfolioById,
