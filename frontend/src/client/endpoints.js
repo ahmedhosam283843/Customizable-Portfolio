c97b52d1dcbcc221369b8523da079ccdf49fea2b
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-04-24 16:18:27 +0200

    feat: api endpoints path

--- frontend/src/client/endpoints.js
+++ frontend/src/client/endpoints.js
@@ -2,6 +2,10 @@ const endpoints = {
     login: "/login",
     register: "/user",
     portfolio: "/portfolio",
+    user: "/user",
+    projects: "/projects",
+    skills: "/skills",
+    experiences: "/experiences",
   };
   
   export default endpoints;
