7f6454a599efe95de2d818b755f4d337f713ac4e
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-05-09 16:25:39 +0300

    feat: add error box in skills form

--- frontend/src/components/dialogs/AddSkillsExperienceDialog/SkillsForm/SkillsForm.jsx
+++ frontend/src/components/dialogs/AddSkillsExperienceDialog/SkillsForm/SkillsForm.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import "./SkillsForm.scss";
 import axios from "../../../../client/axios.js";
 import endpoints from "../../../../client/endpoints.js";
-const SkillsForm = ({handleFormSubmit}) => {
-  const { register, handleSubmit } = useForm();
+const SkillsForm = ({ handleFormSubmit }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [data, setData] = useState();
-
 
   const onSubmit = async (data) => {
     try {
@@ -39,6 +42,17 @@ const SkillsForm = ({handleFormSubmit}) => {
     <div className="skills-form">
       <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
         <div className="container">
+          {/* Display error box from errors*/}
+          {Object.keys(errors).length > 0 && (
+            <div className="alert alert-danger">
+              {Object.values(errors).map(
+                (error) =>
+                  error.message.length > 0 && ( // only display error message if there is one
+                    <p key={error.message}>{"*" + error.message}</p>
+                  )
+              )}
+            </div>
+          )}
           <div className="row mb-4">
             <div className="col-lg-8 col-md-12 col-sm-12">
               <input
@@ -56,22 +70,26 @@ const SkillsForm = ({handleFormSubmit}) => {
                 id="skill-icon"
                 className="form-control custom-input"
                 placeholder="Skill Icon URL"
-                {...register("skill-icon", { required: true })}
+                {...register("skill-icon", {
+                  required: true,
+                  pattern: {
+                    value: /(https?:\/\/.*\.(?:png))/i,
+                    message: "Please enter a valid URL of a PNG image.",
+                  },
+                })}
               />
             </div>
           </div>
         </div>
 
-        
         <div className="d-flex justify-content-center">
           <button type="submit" className="btn customize-btn">
             Add Skill
           </button>
         </div>
-
       </form>
     </div>
   );
-}
+};
 
-export default SkillsForm
+export default SkillsForm;
