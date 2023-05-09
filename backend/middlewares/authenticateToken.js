162140182a82bea84e6e92df570724895161a7d1
ahmedhosam283843 <ahmed.hosam283843@gmail.com>
2023-05-09 16:31:48 +0300

    feat: remove token log

--- backend/middlewares/authenticateToken.js
+++ backend/middlewares/authenticateToken.js
@@ -9,7 +9,6 @@ const authenticateToken = (req, res, next) => {
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
-    console.log(req.user  + " from authenticateToken");
     next();
   });
 };
